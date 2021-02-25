61003c5d093acbd5abc8c0cd9b99a094543a1d01
Jinkyung Lee <summercat@summercatui-MacBookPro.local>
2021-02-25 10:29:45 +0900

    Interaction lib 적용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { MapInteractionCSS } from 'react-map-interaction';
+
 import './App.css';
 
 function App() {
@@ -19,6 +21,7 @@ function App() {
   const [images, setImages] = useState([]);
   // currentId == int
   const [currentId, setCurrentId] = useState(null);
+  const [changeVersion, setChangeVersion] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -32,6 +35,14 @@ function App() {
   const getImage = (id) => images.find((img) => img.id === id);
   const currentImage = getImage(currentId);
 
+  let promptImg;
+  if (!changeVersion) {
+    // promptImg = currentImage.download_url;
+  } else {
+    promptImg = 'https://picsum.photos/seed/picsum/200/300';
+  }
+
+  console.log('image', currentImage);
   return (
     <div className="App">
       <div className="wrap">
@@ -48,17 +59,20 @@ function App() {
         </div>
         <div className="containRight">
           <div className="containerImg">
+            {/* <img src={`${promptImg}`} /> */}
             {currentImage && (
-              <div>
-                <img
-                  className="imageContent"
-                  alt={`image-${currentImage.author}`}
-                  src={`${currentImage.download_url}`}
-                />
+              <div className="imageWrap">
+                <MapInteractionCSS>
+                  <img
+                    className="imageContent"
+                    alt={`image-${currentImage.author}`}
+                    src={`${currentImage.download_url}`}
+                  />
+                </MapInteractionCSS>
               </div>
             )}
           </div>
-          <button>dd</button>
+          <button onClick={() => setChangeVersion(!changeVersion)}>원본</button>
         </div>
       </div>
     </div>
