57e09e612619111678af3399d2b61ce93f26e027
Jinkyung Lee <summercat@summercatui-MacBookPro.local>
2021-02-26 10:05:21 +0900

    wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,15 +20,14 @@ function App() {
   ]
   */
   const [images, setImages] = useState([]);
-  // currentId == int
   const [currentId, setCurrentId] = useState(null);
   const [changeVersion, setChangeVersion] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('http://localhost:8080/pics/');
-      // setImages(result.data);
-      console.log('result', result);
+      const result = await axios.get('https://picsum.photos/v2/list');
+      setImages(result.data);
+      // console.log('result', result);
     };
 
     fetchData();
@@ -37,26 +36,22 @@ function App() {
   const getImage = (id) => images.find((img) => img.id === id);
   const currentImage = getImage(currentId);
 
-  // let promptImg;
-  // if (!changeVersion) {
-  //   promptImg = currentImage.download_url;
-  // } else {
-  //   promptImg = 'https://picsum.photos/seed/picsum/200/300';
-  // }
-
   return (
     <div className="App">
       <div className="wrap">
         <div className="containerList">
-          {images.map((item, idx) => (
-            <div key={idx}>
-              <p
-                onClick={() => {
-                  setCurrentId(item.id);
-                }}
-              >{`image-${item.id}`}</p>
-            </div>
-          ))}
+          {images.map((item, idx) => {
+            console.log('idx', item.id);
+            return (
+              <div key={idx}>
+                <p
+                  onClick={() => {
+                    setCurrentId(item.id);
+                  }}
+                >{`image-${item.id}`}</p>
+              </div>
+            );
+          })}
         </div>
         <div className="containRight">
           <div className="containerImg">
@@ -76,7 +71,7 @@ function App() {
               <Loading />
             )}
           </div>
-          <button onClick={() => setChangeVersion(!changeVersion)}>원본</button>
+          {/* <button onClick={() => doDisplay()}>원본</button> */}
         </div>
       </div>
     </div>
