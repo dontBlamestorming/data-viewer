1fed76584eae1b27fe53f65a0e56773e63a32da5
Jinkyung Lee <summercat@summercatui-MacBookPro.local>
2021-03-02 17:51:10 +0900

    Login: Mockup API로 test

--- frontend/src/Pages/LogIn/LogIn.js
+++ frontend/src/Pages/LogIn/LogIn.js
@@ -1,12 +1,37 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 import './LogIn.css';
 import Button from '@material-ui/core/Button';
-import axios from 'axios';
 
 const LogIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [msg, setMsg] = useState('');
+
+  // const API = 'http://localhost:8080/';
+  const MockAPI = 'https://reqres.in/';
+  const history = useHistory();
+
+  const onClickLogin = () => {
+    return axios
+      .post(MockAPI + 'api/login', {
+        email,
+        password,
+      })
+      .then((response) => {
+        if (response.data.token) {
+          localStorage.setItem(
+            'usertoken',
+            JSON.stringify(response.data.token),
+          );
+          history.push('/');
+        }
+
+        return response.data;
+      });
+  };
 
   return (
     <div className="login-page">
@@ -32,12 +57,15 @@ const LogIn = () => {
         />
         <Button
           className="loginBttn"
-          onClick={() => {}}
+          onClick={() => {
+            onClickLogin();
+          }}
           variant="outlined"
           color="primary"
         >
           LogIn
         </Button>
+        {msg}
       </div>
     </div>
   );
--- frontend/src/component/Header/Header.js
+++ frontend/src/component/Header/Header.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './Header.css';
 import Button from '@material-ui/core/Button';
 
 const Header = () => {
+  const [user, setUser] = useState('');
+  const Token = localStorage.getItem('usertoken');
+
+  const logout = () => {
+    localStorage.clear();
+  };
   return (
     <div className="Header">
       <div className="Wrap">
         <div className="LoginBttn">
-          <Link to="/login" style={{ textDecoration: 'none' }}>
-            <Button variant="outlined" color="primary">
-              Login
-            </Button>
-          </Link>
+          {!Token ? (
+            <Link to="/login" style={{ textDecoration: 'none' }}>
+              <Button variant="outlined" color="primary">
+                Login
+              </Button>
+            </Link>
+          ) : (
+            <Link
+              to="/login"
+              style={{ textDecoration: 'none' }}
+              onClick={logout}
+            >
+              <Button variant="outlined" color="primary">
+                Logout
+              </Button>
+            </Link>
+          )}
         </div>
       </div>
     </div>
