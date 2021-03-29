50e5d37ec67f33928e267555d98595e0d5451823
Woo-cheol Jeong <kaske@summercat.co>
2021-03-29 14:57:03 +0900

    CI bug fix

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -1,5 +1,13 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['222.122.232.80', ]
+ALLOWED_HOSTS = [
+    "222.122.232.80",
+]
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "database" / "db.sqlite3",
+    }
+}
